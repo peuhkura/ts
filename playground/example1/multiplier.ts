eb799b9d500b7310d2748d5663bf281d0330d0bc
Antti Peuhkurinen <peuhkura@gmail.com>
2024-06-12 15:30:23 +0300

    initial calculator example, part 3, multiply works

--- playground/example1/multiplier.ts
+++ playground/example1/multiplier.ts
@@ -2,4 +2,6 @@ const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText,  a * b);
 }
 
-multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
