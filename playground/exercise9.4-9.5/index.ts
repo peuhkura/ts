402d70167ca89fa90f2abaa9d29692962a99cd42
Antti Peuhkurinen <peuhkura@gmail.com>
2024-06-22 23:46:17 +0300

    response OK for 9.5

--- playground/exercise9.4-9.5/index.ts
+++ playground/exercise9.4-9.5/index.ts
@@ -1,13 +1,38 @@
 import express from 'express';
+import { calculateBmi } from './bmicalculator';
 
 const app = express();
+const port = 3003;
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.get('/bmi', (req, res) => {
+
+  try {
+    const tmpCm = req.query.height as string;
+    const tmpKg = req.query.weight as string;
+    const cm = parseFloat(tmpCm);
+    const kg = parseFloat(tmpKg);
+    if (isNaN(cm) || isNaN(kg)) {
+      throw new Error("malformatted parameters");
+    }
+
+    let result: string = '';
+    result = calculateBmi(cm, kg);
+
+    const responseData = {
+        weight: kg,
+        height: cm,
+        bmi: result
+    };
+    res.send(responseData);
+  } catch (error) {
+    const responseData = {
+        error: error.message
+    };
+    res.send(responseData);
+  }
 });
 
-const PORT = 3003;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
