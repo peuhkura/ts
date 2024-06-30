35086c29aa5405e03cdafb66d31e817c80872e81
Antti Peuhkurinen <peuhkura@gmail.com>
2024-06-30 18:02:20 +0300

    patientor listing names OK

--- playground/exercise9.8-9.11/src/index.ts
+++ playground/exercise9.8-9.11/src/index.ts
@@ -8,7 +8,27 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+// Add headers before the routes are defined
+app.use(function (_req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
@@ -17,7 +37,7 @@ app.use('/api/diaries', diaryRouter);
 
 app.use('/api/diagnoses', routerDiagnoses);
 
-app.use('/api/services/patients', routerPatients);
+app.use('/api/patients', routerPatients);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
