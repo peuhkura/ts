eba0ef25dc1b69f9f2a875695998d16d7c9b47a1
Antti Peuhkurinen <peuhkura@gmail.com>
2024-06-30 23:32:42 +0300

    9.14 on-going

--- exercise9.14-9.19/my-app-name/src/App.tsx
+++ exercise9.14-9.19/my-app-name/src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  /*const courseName = "Half Stack application development";
+  
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
 
-function App() {
-  const [count, setCount] = useState(0)
+  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);*/
+
+  
+
+  const course = 'Half Stack application development'
+  interface HeaderProps {
+    course: string;
+  }
+  const Header: React.FC<HeaderProps> = ({ course }) => (
+    <h1>{course}</h1>
+  );
+
+  const exerciseData = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ] 
+  interface ContentProps {
+    parts: string;
+  }
+  const Content: React.FC<ContentProps> = ({ parts }) => (
+    <div>{parts}</div>
+  );
+  const formattedPartsString = exerciseData.join(', ');
+
+  interface TotalProps {
+    parts: number;
+  }
+  const Total: React.FC<TotalProps> = ({ parts }) => (
+    <div>{parts}</div>
+  );
+  const formattedPartsTotal = 12;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content parts={formattedPartsString} />
+      <Total parts={formattedPartsTotal} />
+    </div>
   )
-}
 
-export default App
+  /*return (
+    <div>
+      <Header name={courseName} />
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises {totalExercises}
+      </p>
+    </div>
+  );*/
+};
+
+export default App;
